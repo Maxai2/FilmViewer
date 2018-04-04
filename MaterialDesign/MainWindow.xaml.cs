b2ca29a1f300c9e2ec9b9c136f78e6ad8f3137ab
Maxai <maxali946@gmail.com>
2018-04-04 18:24:54 +0400

    Add files via upload

---
+++ MaterialDesign/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace MaterialDesign
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Main.Content = new LogIn();
+        }
+        private void close_program(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Register();
+        }
+    }
+}
