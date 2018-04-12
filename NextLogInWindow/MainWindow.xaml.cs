6055200a72445d49d87d51e283ac628fd470a911
Maxai <maxali946@gmail.com>
2018-04-12 18:10:28 +0400

    Add files via upload

--- NextLogInWindow/MainWindow.xaml.cs
+++ NextLogInWindow/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+//-------------------------------------------------------------------
 namespace NextLogInWindow
 {
     /// <summary>
@@ -24,5 +24,48 @@ namespace NextLogInWindow
         {
             InitializeComponent();
         }
+        //-------------------------------------------------------------------
+        bool changeCol = false;
+
+        private void PackIcon_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (changeCol)
+            {
+                piGrid.Background = Brushes.Transparent;
+                piList.Background = Brushes.Gray;
+            }
+            else
+            {
+                piGrid.Background = Brushes.Gray;
+                piList.Background = Brushes.Transparent;
+            }
+
+            changeCol = !changeCol;
+        }
+        //-------------------------------------------------------------------
+        private void tbMoreWatched_MouseEnter(object sender, MouseEventArgs e) => tbMoreWatched.TextDecorations = TextDecorations.Underline;
+        //-------------------------------------------------------------------
+        private void tbMoreWatched_MouseLeave(object sender, MouseEventArgs e) => tbMoreWatched.TextDecorations = null;
+        //-------------------------------------------------------------------
+        private void tbMoreWatched_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+        //-------------------------------------------------------------------
+        private void tbMoreLook_MouseEnter(object sender, MouseEventArgs e) => tbMoreLook.TextDecorations = TextDecorations.Underline;
+        //-------------------------------------------------------------------  
+        private void tbMoreLook_MouseLeave(object sender, MouseEventArgs e) => tbMoreLook.TextDecorations = null;
+        //-------------------------------------------------------------------
+        private void tbMoreLook_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void TextBox_FocusableChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+
+        }
+        //-------------------------------------------------------------------
     }
 }
+//-------------------------------------------------------------------
