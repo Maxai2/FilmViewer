44838bc8ea29d3418603a94c034350eb9d55663c
Maxai <maxali946@gmail.com>
2018-04-13 11:45:56 +0400

    Add files via upload

---
+++ NextLogInWindow/Film.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//------------------------------------------
+namespace NextLogInWindow
+{
+    public class Film
+    {
+        public string Title { get; set; }
+        public string Year { get; set; }
+        public string Runtime { get; set; }
+        public string Genre { get; set; }
+        public string Plot { get; set; }
+        public string imdbID { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
+//------------------------------------------
--- NextLogInWindow/MainWindow.xaml.cs
+++ NextLogInWindow/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,31 @@ namespace NextLogInWindow
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<Film> WatchedFilm { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+
+            lbWatchedFilms.ItemTemplate = this.Resources["lbItemsPanelWrap"] as DataTemplate;
+            lbWatchedFilms.ItemsPanel = this.Resources["lbItemTemplateGrid"] as ItemsPanelTemplate;
+
+            WatchedFilm = new ObservableCollection<Film>();
+
+            Film film1 = new Film
+            {
+                Title = "The Lord of the Rings: The Fellowship of the Ring",
+                Year = "2001",
+                Runtime = "178 min",
+                Genre = "Adventure, Drama, Fantasy",
+                Plot = "A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from",
+                imdbID = "tt0120737",
+                ImagePath = "https://ia.media-imdb.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg"
+            };
+
+            WatchedFilm.Add(film1);
+            WatchedFilm.Add(film1);
+            WatchedFilm.Add(film1);
         }
         //-------------------------------------------------------------------
         bool changeCol = false;
@@ -33,11 +56,17 @@ namespace NextLogInWindow
             {
                 piGrid.Background = Brushes.Transparent;
                 piList.Background = Brushes.Gray;
+
+                lbWatchedFilms.ItemTemplate = this.Resources["lbItemsPanelStack"] as DataTemplate;
+                lbWatchedFilms.ItemsPanel = this.Resources["lbItemTemplateList"] as ItemsPanelTemplate;
             }
             else
             {
                 piGrid.Background = Brushes.Gray;
                 piList.Background = Brushes.Transparent;
+
+                lbWatchedFilms.ItemTemplate = this.Resources["lbItemsPanelWrap"] as DataTemplate;
+                lbWatchedFilms.ItemsPanel = this.Resources["lbItemTemplateGrid"] as ItemsPanelTemplate;
             }
 
             changeCol = !changeCol;
